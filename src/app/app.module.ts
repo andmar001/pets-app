019afaa29da79d5d250aa73840da144fa2efb9ad
tony tolver <andrademaresantonio00@gmail.com>
2023-01-28 18:11:19 -0600

    Estructura principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { VerMascotasComponent } from './components/ver-mascotas/ver-mascotas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarEditarMascotaComponent,
+    ListadoMascotaComponent,
+    VerMascotasComponent
   ],
   imports: [
     BrowserModule,
