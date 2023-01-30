7684e36be567f76f7afe5edc3359ac98e34062a7
tony tolver <andrademaresantonio00@gmail.com>
2023-01-30 00:22:17 -0600

    ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { VerMascotasComponent } from './components/ver-mascotas/ver-mascotas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listado-mascotas', pathMatch: 'full' },
+  { path: 'listado-mascotas', component: ListadoMascotaComponent },
+  { path: 'agregar-mascota', component: AgregarEditarMascotaComponent },
+  { path: 'ver-mascota/:id', component: VerMascotasComponent },
+  { path: 'editar-mascota/:id', component: AgregarEditarMascotaComponent },
+  { path: '**', redirectTo: 'listado-mascotas', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
